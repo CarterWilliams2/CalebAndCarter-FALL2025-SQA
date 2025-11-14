4359785ff33ed197e3d7cfff094427d35d32590f
chf0019 <156932294+chf0019@users.noreply.github.com>
2025-11-14 11:08:04 -0600

    Add logging comments 1 and 2

--- MLForensics-farzana/mining/mining.py
+++ MLForensics-farzana/mining/mining.py
@@ -21,6 +21,7 @@ def deleteRepo(dirName, type_):
     try:
         if os.path.exists(dirName):
             shutil.rmtree(dirName)
+          #log-structure change-repo deleted
     except OSError:
         print('Failed deleting, will try manually')  
         
@@ -40,7 +41,8 @@ def makeChunks(the_list, size_):
 def cloneRepo(repo_name, target_dir):
     cmd_ = "git clone " + repo_name + " " + target_dir 
     try:
-       subprocess.check_output(['bash','-c', cmd_])    
+       subprocess.check_output(['bash','-c', cmd_]) 
+      #log-structure change-repo cloned
     except subprocess.CalledProcessError:
        print('Skipping this repo ... trouble cloning repo:', repo_name )
 
