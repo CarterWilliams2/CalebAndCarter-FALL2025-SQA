c68ee834151777e0b75e0e4e172358368bb47afc
chf0019 <156932294+chf0019@users.noreply.github.com>
2025-12-01 14:25:54 -0600

    Add sys import and update path for MLForensics

--- fuzz.py
+++ fuzz.py
@@ -2,6 +2,7 @@ import statistics
 from datetime import date
 import os
 import unittest
+import sys
 sys.path.insert(0, "MLForensics-farzana")
 from mining import getPythonFileCount
 #methods to fuzz -----------------------
